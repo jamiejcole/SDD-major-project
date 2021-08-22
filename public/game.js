9fc0da5aaface26291e950413cc57d6794f16bf9
Jamie Cole <jamiejcole@icloud.com>
2021-08-22 20:19:32 +1000

    finalised versions of html and js

--- public/game.js
+++ public/game.js
@@ -1,53 +1,6 @@
-// https://www.youtube.com/watch?v=3SsYZDJdeXk&ab_channel=KnifeCircus 9:57
-/*
-var block = document.getElementById("block");
-var hole = document.getElementById("hole");
-var character = document.getElementById("character");
-var jumping = 0;
-var counter = 0;
+// This code is inspired from this video: https://www.youtube.com/watch?v=3SsYZDJdeXk&ab_channel=KnifeCircus
 
-hole.addEventListener('animationiteration', () => {
-    var random = Math.random()*3;
-    var top = (random * 100) + 150;
-    hole.style.top = -(top) + "px";
-    counter++;
-});
-
-setInterval(function(){
-    var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"))
-    if (jumping == 0) {
-        character.style.top = (characterTop + 3) + "px";
-    }
-
-    var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"))
-    var holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"))
-    var cTop = -(500-characterTop);
-
-    if(characterTop > 580 || ((blockLeft < 20) && (blockLeft > -50) && ((cTop < holeTop) || (cTop > holeTop + 130)))) {
-        alert('poo: ' + counter);
-        character.style.top = 100 + "px";
-        counter = 0;
-    }``
-}, 10);
-
-function jump() {
-    jumping = 1;
-    let jumpCount = 0;
-    var jumpInterval = setInterval(function(){
-        var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"))
-        if ((characterTop > 6)&&(jumpCount < 15)) {
-            character.style.top = (characterTop - 5) + "px";
-        }
-        if (jumpCount > 20) {
-            clearInterval(jumpInterval);
-            jumping = 0;
-            jumpCount = 0;
-        }
-        jumpCount ++;
-    }, 10);
-}
-*/
-
+// Setting variables
 var block = document.getElementById("block");
 var hole = document.getElementById("hole");
 var character = document.getElementById("character");
@@ -57,6 +10,7 @@ var jumping = 0;
 var counter = 0;
 var highCounter = 0;
 
+// Cookie function to get the value of the current highscore cookie
 function getCookie(cname) {
     let name = cname + "=";
     let decodedCookie = decodeURIComponent(document.cookie);
@@ -75,6 +29,7 @@ function getCookie(cname) {
 
 highText.innerText = 'Highscore: ' + getCookie('hs');
 
+// Adding an event listener to each iteration of the animation (i.e. when the bar gets generated)
 hole.addEventListener('animationiteration', () => {
     var random = -((Math.random()*300)+150);
     hole.style.top = random + "px";
@@ -82,6 +37,8 @@ hole.addEventListener('animationiteration', () => {
     countText.innerText = 'Score: ' + counter;
     highText.innerText = 'Highscore: ' + getCookie('hs');
 });
+
+// main function that occurs every frame
 setInterval(function(){
     var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     if(jumping==0){
@@ -90,17 +47,16 @@ setInterval(function(){
     var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
     var holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
     var cTop = -(500-characterTop);
+    // Check to see whether the character is in the hole or not.
     if((characterTop>580)||((blockLeft<20)&&(blockLeft>-50)&&((cTop<holeTop + 100)||(cTop>holeTop+230)))){
-        //alert("Game over. Score: "+(counter-1));
         character.style.top = 100 + "px";
-        //console.log('counter, highcounter: ' + counter + highCounter);
+        // Check to see if the highscore is greater than the current score
         if (counter >= highCounter && counter >= getCookie('hs')) {
             console.log("highcuonter: " + highCounter);
-            //console.log('coutner is currently: 0' + counter);
+            console.log('coutner is currently: 0' + counter);
             highCounter = counter;
             highText.innerText = 'Highscore: ' + highCounter;
             document.cookie = "hs="+highCounter+"; expires=Thu, 18 Dec 2100 12:00:00 UTC";
-            //console.log('IF counter, highcounter: ' + counter + highCounter);
         }
         counter=0;
         console.log("highcuonter: " + highCounter);
@@ -108,6 +64,8 @@ setInterval(function(){
     
 },10);
 
+
+// Main jump function that activates whenever the user clicks anything on the game div
 function jump(){
     jumping = 1;
     let jumpCount = 0;
